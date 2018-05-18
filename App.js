f7cd8867fa538043e16743832065b584bc1175c8
Daniel Marczydło <daniel.marczydlo@gmail.com>
2018-05-18 13:08:45 +0200

    splash screen implemented

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Provider} from 'react-redux'
 import configureStore from './configureStore'
 import Index from './pages/Index';
+import SplashScreen from 'react-native-splash-screen';
 
 const store = configureStore();
 
-export default App = () => {
-    return (
-        <Provider store={store}>
-            <Index />
-        </Provider>
-    );
+export default class App extends Component {
+
+    componentDidMount() {
+        SplashScreen.hide();
+    }
+
+    render() {
+        return (
+            <Provider store={store}>
+                <Index/>
+            </Provider>
+        );
+    }
 }
 
 
